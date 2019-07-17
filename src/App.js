43a1db053325ef98d41e9ad4afb890e083551851
IDM.air <innanadm8@gmail.com>
2019-07-17 03:49:49 -0400

    adding components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Button from "./components/Buttons";
 import InfoCard from "./components/InfoCard";
 import Badge from "./components/Badges";
 import Checkbox from "./components/CheckBoxes";
+import Chipbox from "./components/Chipbox";
 import Pagination from "./components/Pagination";
 import Indeterminate from "./components/Indeterminate";
 
@@ -17,6 +18,7 @@ function App() {
       <InfoCard />
       <Badge />
       <Checkbox />
+      <Chipbox />
       <Indeterminate />
       <Pagination />
     </div>
---
+++ src/components/Chipbox.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Chipbox = () => {
+  return (
+    <div class="chip">
+      <strong>WARNING:</strong> DO NOT PRESS
+      <i class="close material-icons">close</i>
+    </div>
+  );
+};
+
+export default Chipbox;
