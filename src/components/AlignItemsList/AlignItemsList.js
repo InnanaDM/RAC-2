c34e8eed8a5f64a72bf5e2e5c8113a21c6e6062e
IDM.air <innanadm8@gmail.com>
2019-07-31 18:25:58 -0400

    adding individual css

---
+++ src/components/AlignItemsList/AlignItemsList.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { images } from "./alignitemslist.css";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  },
+  inline: {
+    display: "inline"
+  }
+}));
+
+export default function AlignItemsList() {
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="FU2" src="public/images/STATIC.png" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Ten components?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                check
+              </Typography>
+              {" Took me way longer than it should've"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="WTF" src="public/images/STATIC.png" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Using a CSS framework?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                check
+              </Typography>
+              {" I absolutely love Material-React <3"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>{images}</ListItemAvatar>
+        <ListItemText
+          primary="I'M READY FOR CHALLENGE TWO"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                pending...
+              </Typography>
+              {"deadlines are important for my success"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+  );
+}
